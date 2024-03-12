911dd339a7efbef2bdee066fd7578ed53196c85e
Denys Brahin <denisbragin52@gmail.com>
2024-03-12 15:11:34 +0200

    First commit

---
+++ WebsiteParsing.Domain/Entities/Car.cs
@@ -0,0 +1,14 @@
+﻿using Abp.Timing;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebsiteParsing.Domain.Entities
+{
+    public class Car
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string ModelName { get; set; }
+        public DateTimeRange DateRange {  get; set; }
+        public ICollection<Equipment> Equipment {  get; set; }
+    }
+}
---
+++ WebsiteParsing.Domain/Entities/Equipment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsiteParsing.Domain.Entities
+{
+    public class Equipment
+    {
+        [Key]
+        public Guid Id {  get; set; }
+        public string EquipmentName { get; set; }
+        public string Engine { get; set; }
+        public string Body {  get; set; }
+        public string Grade {  get; set; }
+        public string Transmission { get; set; }
+        public string GearShiftType { get; set; }
+        public string Cab {  get; set; }
+        public string TransmissionModel {  get; set; }
+        public string LoadingCapacity {  get; set; }
+
+        public Guid CarId { get; set; }
+        [ForeignKey("CarId")]
+        public Car Car { get; set; }
+
+
+    }
+}
