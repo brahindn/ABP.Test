befc8ad12601d1f467c6d4c2761f98ff9c78cf59
Denys Brahin <denisbragin52@gmail.com>
2024-03-15 09:39:58 +0200

    parsing does not work

--- WebsiteParsing.Console/Program.cs
+++ WebsiteParsing.Console/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Collections.Generic;
 using WebsiteParsing;
 using WebsiteParsing.Application.Services.Contracts;
+using static System.Net.WebRequestMethods;
 
 var startup = new Startup();
 var services = new ServiceCollection();
@@ -11,6 +14,10 @@ var serviceProvider =  services.BuildServiceProvider();
 var serviceManager = serviceProvider.GetRequiredService<IServiceManager>();
 
 await TestDataInDataBase();
+
+var mainAddress = "https://www.ilcats.ru/toyota/?function=getModels&market=EU";
+
+var dataCar = GetDataFromModelCar(url: mainAddress);
 
 
 Console.ReadLine();
@@ -38,4 +45,60 @@ async Task TestDataInDataBase()
     }
 
     Console.WriteLine("Test method is done");
+}
+
+
+static Dictionary<string, string> GetDataFromModelCar(string url)
+{
+    var carProperties = new Dictionary<string, string>();
+
+    try
+    {
+        using (HttpClientHandler handler = new HttpClientHandler { AllowAutoRedirect = false, AutomaticDecompression = System.Net.DecompressionMethods.All })
+        {
+            using (var client = new HttpClient(handler))
+            {
+                using (HttpResponseMessage response = client.GetAsync(url).Result)
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var html = response.Content.ReadAsStringAsync().Result;
+
+                        if (!string.IsNullOrEmpty(html))
+                        {
+                            HtmlAgilityPack.HtmlDocument document = new HtmlAgilityPack.HtmlDocument();
+                            document.LoadHtml(html);
+
+                            var carList = document.DocumentNode.SelectNodes(".//div[@class='ifMultilistBody']//div[@class='List Multilist']/div[@class='Column']");   //Чому не працює?!
+
+                            if(carList != null && carList.Count > 0)
+                            {
+                                foreach(var list in carList)
+                                {
+                                    var cars = list.SelectNodes(".//div");
+
+                                    if (cars != null && cars.Count > 0)
+                                    {
+                                        foreach(var car in cars)
+                                        {
+                                            var carModelName = car.SelectSingleNode(".//div[@class='Header']").InnerText;
+                                            var carModelCode = car.SelectSingleNode(".//div[@class='List ']//div[@class='id']").InnerText;
+
+                                            carProperties.Add(carModelName, carModelCode);
+                                        }
+                                    } 
+                                }
+                            }                                
+                        }
+                    }
+                }
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message.ToString());
+    }
+
+    return carProperties;
 }
